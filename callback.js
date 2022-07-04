bd1f65f87226e8baa9a11d6537a400436b849888
jinhlee19 <jinhlee19@gmail.com>
2022-07-04 23:17:12 +0900

    .

--- callback.js
+++ callback.js
@@ -1,13 +1,13 @@
 const addSum = (a, b, callback) => {
-	setTimeout(function () {
-		if (typeof a !== 'number' || typeof b !== 'number') callback('a and b must be numbers');
+	setTimeout(() => {
+		if (typeof a !== 'number' || typeof b !== 'number') return callback('a and b must be numbers');
 		callback(undefined, a + b);
 	}, 3000);
 };
 
 // error나 sum을 받는 callback
 let callback = (error, sum) => {
-	if (error) console.log({ error });
+	if (error) return console.log({ error });
 	console.log({ sum });
 };
 
@@ -19,3 +19,5 @@ addSumb(10, 12, callback);
 // 		callback(undefined, a + b);
 // 	}, 3000);
 // };
+
+// callback은 그냥 함수 이름이며, 지금과 같이 non blocking 작업이 끝나고 나서 호출하는 함수를 이렇게 부를 뿐.
