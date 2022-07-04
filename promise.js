816e1f27ad0b5dcc297d9fbe7b2b84825be1d876
jinhlee19 <jinhlee19@gmail.com>
2022-07-04 23:55:10 +0900

    callback chain

--- promise.js
+++ promise.js
@@ -1,14 +1,17 @@
-const addSum = (a, b) => {
-	return new Promise((resolve, reject) => {
+const addSum = (a, b) =>
+	new Promise((resolve, reject) => {
 		setTimeout(() => {
 			if (typeof a !== 'number' || typeof b !== 'number') {
 				reject('a, b must be numbers');
 			}
 			resolve(a + b);
-		}, 3000);
+		}, 1000);
 	});
-};
 
 addSum(10, 20)
+	.then(sum1 => addSum(sum1, 3))
+	.then(sum1 => addSum(sum1, 3))
+	.then(sum1 => addSum(sum1, 3))
+	.then(sum1 => addSum(sum1, 3))
 	.then(sum => console.log({ sum }))
 	.catch(error => console.log({ error }));
