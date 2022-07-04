6ba9c89bea89f80f2bcc57320f76c8e03c279ecc
jinhlee19 <jinhlee19@gmail.com>
2022-07-05 00:00:58 +0900

    async await

--- promise.js
+++ promise.js
@@ -8,10 +8,22 @@ const addSum = (a, b) =>
 		}, 1000);
 	});
 
-addSum(10, 20)
-	.then(sum1 => addSum(sum1, 3))
-	.then(sum1 => addSum(sum1, 3))
-	.then(sum1 => addSum(sum1, 3))
-	.then(sum1 => addSum(sum1, 3))
-	.then(sum => console.log({ sum }))
-	.catch(error => console.log({ error }));
+// addSum(10, 20)
+// 	.then(sum1 => addSum(sum1, 3))
+// 	.then(sum1 => addSum(sum1, 3))
+// 	.then(sum1 => addSum(sum1, 3))
+// 	.then(sum1 => addSum(sum1, 3))
+// 	.then(sum => console.log({ sum }))
+// 	.catch(error => console.log({ error }));
+
+const totalSum = async () => {
+	try {
+		let sum = await addSum(10, 10);
+		let sum2 = await addSum(sum, 10);
+		console.log({ sum, sum2 });
+	} catch (error) {
+		console.log(error);
+	}
+};
+// console.log(totalSum());
+totalSum();
