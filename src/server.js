7004547d5d1fca3331affa77cd55f25c8545f2c9
jinhlee19 <jinhlee19@gmail.com>
2022-07-07 16:20:54 +0900

    git 2

--- src/server.js
+++ src/server.js
@@ -53,15 +53,19 @@ const server = async () => {
 
 		app.put('/user/:userId', async (req, res) => {
 			try {
-				// console.log(req.params);
 				const { userId } = req.params;
-				if (!mongoose.isValidObjectId(userId)) {
-					return res.status(400).send({ err: 'Invalid userId.' });
-				}
-				const { age } = req.body;
-				// if (typeof age !== 'number') return res.status(400).send({ err: 'age must be a number' });
-				//  여기서 new: true 는 출력 이전에 업데이트 반영을 위함.
-				const user = await User.findByIdAndUpdate(userId, { $set: { age } }, { new: true });
+				if (!mongoose.isValidObjectId(userId)) return res.status(400).send({ err: 'Invalid userId.' });
+				const { age, name } = req.body;
+				if (!age && !name) return res.status(400).send({ err: 'age or name is required' });
+				if (age && typeof age !== 'number') return res.status(400).send({ err: 'age must be a number' });
+				if (name && typeof name.first !== 'string' && typeof name.last !== 'string')
+					return res.status(400).send({ err: 'first and last name must be strings' });
+				let updateBody = {};
+				// 수업에서는 빈 부분을 postman에 PUT req 시, 빈 부분이 null 값을 받으나, 현재 버전 기준에서는 이런 오류가 발생하지 않는다. 아래 코드는 이전버전에 제한되는건가?
+				// if (age) updateBody.age = age;
+				// if (name) updateBody.name = name;
+
+				const user = await User.findByIdAndUpdate(userId, { age, name }, { new: true });
 				return res.send({ user });
 			} catch (err) {
 				return res.status(500).send({ msg: err.message });
