97c0016913201f2a3b79e55bef541412c9b220f5
jinhlee19 <jinhlee19@gmail.com>
2022-07-17 06:16:10 +0900

    Blog Api

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -26,6 +26,8 @@ blogRouter.post('/', async (req, res) => {
 
 blogRouter.get('/', async (req, res) => {
 	try {
+		const blogs = await Blog.find({});
+		return res.send({ blogs });
 	} catch (err) {
 		console.log(err);
 		res.status(500).send({ err: err.message });
@@ -34,6 +36,10 @@ blogRouter.get('/', async (req, res) => {
 
 blogRouter.get('/:blogId', async (req, res) => {
 	try {
+		const { blogId } = req.params;
+		if (!isValidObjectId(blogId)) res.status(400).send({ err: 'blogId is invalid.' });
+		const blog = await Blog.findOne({ _id: blogId });
+		return res.send({ blog });
 	} catch (err) {
 		console.log(err);
 		res.status(500).send({ err: err.message });
@@ -42,6 +48,15 @@ blogRouter.get('/:blogId', async (req, res) => {
 
 blogRouter.put('/:blogId', async (req, res) => {
 	try {
+		const { blogId } = req.params;
+		if (!isValidObjectId(blogId)) res.status(400).send({ err: 'blogId is invalid.' });
+
+		const { title, content } = req.body;
+		if (typeof title !== 'string') res.status(400).send({ err: 'title is required' });
+		if (typeof content !== 'string') res.status(400).send({ err: 'content is required' });
+
+		const blog = await Blog.findOneAndUpdate({ _id: blogId }, { title, content }, { new: true });
+		return res.send({ blog });
 	} catch (err) {
 		console.log(err);
 		res.status(500).send({ err: err.message });
@@ -51,6 +66,13 @@ blogRouter.put('/:blogId', async (req, res) => {
 // 부분수정
 blogRouter.patch('/:blogId/live', async (req, res) => {
 	try {
+		const { blogId } = req.params;
+		if (!isValidObjectId(blogId)) res.status(400).send({ err: 'blogId is invalid.' });
+		const { islive } = req.body;
+		if (typeof islive !== 'boolean') res.status(400).send({ err: 'islive is required.' });
+
+		const blog = await Blog.findOneAndUpdate(blogId, { islive }, { new: true });
+		return res.send({ blog });
 	} catch (err) {
 		console.log(err);
 		res.status(500).send({ err: err.message });
